978d3ea5359b9f4ab58be16bcc4982a7b57ab977
maha-codes <108022396+maha-codes@users.noreply.github.com>
2023-03-19 23:37:58 +0530

    Delete instausers.sql

--- instausers.sql
+++
@@ -1,4 +0,0 @@
-#A) Marketing 
-#1) Rewarding Most Loyal Users: People who have been using the platform for the longest time.
-#Your Task: Find the 5 oldest users of the Instagram from the database provided.
-SELECT username, created_at FROM users ORDER BY created_at limit 5;
